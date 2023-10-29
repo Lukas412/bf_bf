655e174af6ad0d1a7cb778bd18c0626991676da1
lukas <lukas.weber.4.12@gmail.com>
2023-10-29 14:42:26 +0100

    wip

--- bf_bf/src/main.rs
+++ bf_bf/src/main.rs
@@ -14,3 +14,62 @@ fn main() {
         print!("{}", next as char);
     }
 }
+
+struct Bruteforce {
+    runtime: Runtime,
+    result: String,
+    state: Vec<BfState>,
+}
+
+impl Bruteforce {
+    const PIECES: &'static str = ".[]+-<>";
+
+    fn new(result: String) -> Self {
+        Self {
+            runtime: Runtime::new(),
+            result,
+            state: Vec::new(),
+        }
+    }
+
+    fn find_next_vertical(&mut self, finish_execution: bool) -> String {
+        todo!();
+        // self.runtime.code().to_owned()
+    }
+
+    fn find_next_horizontal(&mut self, finish_execution: bool, max_length: usize) -> String {
+        self.runtime.code().to_owned()
+    }
+}
+
+struct BfState {
+    state: Vec<BfStatePiece>,
+    open_loops: usize,
+    code: String,
+}
+
+impl BfState {
+    fn new() -> Self {
+        Self {
+            state: Vec::new(),
+            open_loops: 0,
+            code: String::new(),
+        }
+    }
+
+    fn next(&mut self) -> &str {
+        let Some(last) = self.state.last() else {
+            self.state.push(BfStatePiece::Print);
+            return self.generate();
+        };
+    }
+}
+
+enum BfStatePiece {
+    Print,
+    LoopStart,
+    Increase(usize),
+    Decrease(usize),
+    MoveNext,
+    MovePrevious,
+}
--- bf_run/src/lib.rs
+++ bf_run/src/lib.rs
@@ -1,5 +1,6 @@
 use itertools;
 use itertools::repeat_n;
+use std::mem;
 
 pub struct Runtime {
     cells: Vec<u8>,
@@ -35,7 +36,11 @@ impl Runtime {
         }
     }
 
-    pub fn next_output<'a>(&'a mut self) -> Option<u8> {
+    pub fn code(&self) -> &str {
+        self.code.as_str()
+    }
+
+    pub fn next_output(&mut self) -> Option<u8> {
         loop {
             match self.code.chars().nth(self.code_ptr)? {
                 '>' => {
@@ -78,24 +83,46 @@ impl Runtime {
         }
     }
 
+    #[inline]
     pub fn set_code(&mut self, code: &str) -> Result<(), ()> {
-        self.cells.iter_mut().for_each(|bit| *bit = 0);
-        self.ptr = 0;
-        self.code_ptr = 0;
-
-        self.jumps.clear();
-        self.jumps_map.clear();
-
         self.code.clear();
         self.code.push_str(code);
+        self.reset_execution();
+        self.recalculate_jumps(0, true)
+    }
 
-        self.jumps.iter_mut().for_each(|jump| *jump = None);
+    #[inline]
+    pub fn remove_append_code(&mut self, remove: usize, append: &str) -> Result<(), ()> {
+        let prev = self.code.len();
+        self.code.push_str(append);
+        if append.contains(['[', ']']) {
+            return self.recalculate_jumps(prev, false);
+        }
+        Ok(())
+    }
+
+    #[inline]
+    pub fn append_code(&mut self, code: &str) -> Result<(), ()> {
+        let prev = self.code.len();
+        self.code.push_str(code);
+        if code.contains(['[', ']']) {
+            return self.recalculate_jumps(prev, false);
+        }
+        Ok(())
+    }
+
+    #[inline]
+    fn recalculate_jumps(&mut self, start: usize, remove_old: bool) -> Result<(), ()> {
+        if remove_old {
+            self.jumps.iter_mut().for_each(|jump| *jump = None);
+        }
         if self.code.len() > self.jumps.len() {
             self.jumps
                 .extend(repeat_n(None, self.code.len() - self.jumps.len()));
         }
 
-        for (index, instruction) in self.code.chars().enumerate() {
+        self.jumps_map.clear();
+        for (index, instruction) in self.code[start..].chars().enumerate() {
             match instruction {
                 '[' => self.jumps_map.push(index),
                 ']' => {
@@ -108,8 +135,14 @@ impl Runtime {
                 _ => {}
             }
         }
-
         self.jumps_map.clear();
         Ok(())
+    }
+
+    #[inline]
+    pub fn reset_execution(&mut self) {
+        self.cells.iter_mut().for_each(|bit| *bit = 0);
+        self.ptr = 0;
+        self.code_ptr = 0;
     }
 }
